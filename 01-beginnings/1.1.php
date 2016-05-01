580a7fbba0a20b069421af5f1aba423ea61aebe8
Jimmy <jgs2017@gmail.com>
2016-05-01 14:06:09 -0500

    complete 1.1

---
+++ 01-beginnings/1.1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>1.1</title>
+</head>
+<body>
+<?php 
+//display Hello World! in the browser
+
+echo "Hello World!";
+?>
+</body>
+</html>
