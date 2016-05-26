c936912b9a9f0c89d4db40ea751ee251c4f03749
Jimmy <jgs2017@gmail.com>
2016-05-26 10:19:53 -0500

    complete 3.1

---
+++ 03-forms/3.1/3.1.2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset=utf-8 />
+<title>3.1</title>
+</head>
+<body>
+
+<?php
+$name = $_POST['name'];
+?>
+
+Your favorite city is <?php echo $name; ?>
+
+</body>
+</html>
